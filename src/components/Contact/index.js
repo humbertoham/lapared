05ca2f0c1f568368fc7f3bc4631a8192b4cd263f
Humberto Ham <hamduranhumberto@gmail.com>
2021-11-06 18:44:38 -0500

    grammar contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,24 +1,31 @@
-import { faFacebookF, faInstagram, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import {
+  faFacebookF,
+  faInstagram,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
-import { Content, Wrapper, Text, Message} from "./Contact.styles";
-const Contact = () =>(
-
-    <Wrapper id="contact">
-        <Content className="foot">
-            <Text>
-                <h1>contactanos</h1>
-            </Text>
-            <Message href="https://www.facebook.com/laparedlol"> <FontAwesomeIcon icon={(faFacebookF)}/> enviar mensaje </Message>
-            <Message href="https://www.instagram.com/laparedlol"> <FontAwesomeIcon icon={(faInstagram)}/> enviar mensaje </Message>
-            <Message href="https://www.twitter.com/laparedlol" > <FontAwesomeIcon icon={(faTwitter)}/> enviar mensaje </Message>
-            </Content>
-    </Wrapper>
-
-
-
-
+import { Content, Wrapper, Text, Message } from "./Contact.styles";
+const Contact = () => (
+  <Wrapper id="contact">
+    <Content className="foot">
+      <Text>
+        <h1>contáctanos</h1>
+      </Text>
+      <Message href="https://www.facebook.com/laparedlol">
+        {" "}
+        <FontAwesomeIcon icon={faFacebookF} /> enviar mensaje{" "}
+      </Message>
+      <Message href="https://www.instagram.com/laparedlol">
+        {" "}
+        <FontAwesomeIcon icon={faInstagram} /> enviar mensaje{" "}
+      </Message>
+      <Message href="https://www.twitter.com/laparedlol">
+        {" "}
+        <FontAwesomeIcon icon={faTwitter} /> enviar mensaje{" "}
+      </Message>
+    </Content>
+  </Wrapper>
 );
-
 
 export default Contact;
